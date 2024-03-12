79ff16f71c7a15d592c6cde2a901aaf06fe68909
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-03-12 17:20:05 +0400

    Classworks added

---
+++ ClassWork/ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+//int a = int.Parse(Console.ReadLine());
+//int num = 1;
+
+
+//if (num>0)
+//{
+//    int b = 25;
+//    Console.WriteLine("eden musbetdir");
+//}
+//else if (num == 0)
+//{
+//    Console.WriteLine("Neytral");
+//}
+//else
+//{
+//    Console.WriteLine("eded menfidir");
+//}
+
+//ctrl + k +c (u)
+
+int num = 52;
+
+int a = num % 10;
+int b = num / 10;
+
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+
+
+//if (num%10>num/10)
+//{
+//    Console.WriteLine(num%10);
+//}
+//else
+//{
+//    Console.WriteLine(num/10);
+//}
+
