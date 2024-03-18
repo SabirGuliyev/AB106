92d06ed2f84de07143e8c21ad4702db4f7409b9d
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-03-18 16:07:34 +0400

    Home solution and method classwork added

---
+++ CLasswork18.-3/CLasswork18.-3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CLasswork18._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 3, 15, 20, 5, 7 };
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+              
+                if (CheckNum(arr[i]))
+                {
+                    count++;
+                }
+            }
+
+
+            Console.WriteLine(count);
+        }
+
+        public static bool CheckNum(int num)
+        {
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+            //if (num%2==0)
+            //{
+            //    return true;
+            //}
+            //return false;
+
+        }
+
+       
+    }
+}
---
+++ HomeSolution18.03/HomeSolution18.03/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+
+//int num = 33;
+//int power = 0;
+//int value = 1;
+//int save = num;
+
+//while (save/2!=0)
+//{
+//    save /= 2;
+//    value *= 2;
+//    power++;
+//}
+
+//if (value-num>(value/2)-num)
+//{
+//    Console.WriteLine(++power);
+//}
+//else
+//{
+//    Console.WriteLine(power);
+//}
+
+
+//int num = 23;
+//int power = 0;
+//int value = 1;
+
+//while (value<num)
+//{
+//    value *= 2;
+//    power++;
+//}
+
+//if (value-num>num-(value/2))
+//{
+//    Console.WriteLine(--power);
+//}
+//else
+//{
+//    Console.WriteLine(power);
+//}
