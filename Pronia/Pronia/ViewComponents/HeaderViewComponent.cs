d8778f348ea848e499458bedbb7a78bfd7431dd8
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-06-03 15:11:59 +0400

    Pronia added

---
+++ Pronia/Pronia/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pronia.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
